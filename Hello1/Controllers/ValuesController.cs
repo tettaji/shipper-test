0e9d0c42608b721d9786d813c3ef38118eb013ee
Tariq Ettaji <tettaji@xebia.com>
2018-12-14 10:52:10 +0100

    Init

---
+++ Hello1/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hello1.Controllers
+{
+    [Route("api/hello")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello1";
+        }
+    }
+}
